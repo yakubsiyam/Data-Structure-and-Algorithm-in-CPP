b12cb50269c0719c92b8216ce57a919a0ddbc3f8
yakubsiyam <itsyakubsiyam@gmail.com>
2023-03-29 10:46:28 +0600

    Adding Taylor series Iterative method

---
+++ Taylor Series Iterative.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double e(int x, int n)
+{
+    double s = 1;
+    double num = 1;
+    double den = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        num *= x;
+        den *= i;
+        s += num / den;
+    }
+    return s;
+}
+int main()
+{
+    cout << e(1, 10) << endl;
+    return 0;
+}
